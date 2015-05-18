5c536451b8f88f31ba7874677e2b4cff5b1ff2a0
elquatro <elquatro@ya.ru>
2015-05-18 10:05:49 +0300

    app.js fix

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var LENGTH = 10;
 var app = angular.module('radiotQuotesApp', []);
 
 app.controller('Quote', function($scope, $http, $timeout) {
+
+    var currentQuoteId = undefined;
+
     $scope.getRandomQuote = function() {
         if ($scope.moreDisabled === true) return;
 
-        var quoteId = Math.floor(Math.random() * LENGTH),
-            url = 'json/' + zfill(quoteId, 5) + '.json',
+        var quoteId;
+
+        for (quoteId=Math.floor(Math.random() * LENGTH);
+             currentQuoteId!=undefined && quoteId==currentQuoteId && LENGTH>1;
+             quoteId=Math.floor(Math.random() * LENGTH)) {
+            console.log(quoteId);
+        }
+
+        var url = 'json/' + zfill(quoteId, 5) + '.json',
             loading = $timeout(function() {
                 $scope.quote = undefined;
             }, 100);
@@ -15,6 +25,7 @@ app.controller('Quote', function($scope, $http, $timeout) {
         $scope.moreDisabled = true;
 
         $http.get(url).success(function(data, status, headers, config) {
+            currentQuoteId = quoteId;
             $scope.moreDisabled = false;
             $timeout.cancel(loading);
             $scope.quote = data;
