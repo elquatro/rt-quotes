d00616040e620eafb2b3b4e2816344b0eaa09779
elquatro <elquatro@ya.ru>
2016-02-15 15:50:13 +0300

    refactor

--- app.js
+++ app.js
@@ -1,39 +1,91 @@
 var LENGTH = 66;
 
-angular
-    .module('radiotQuotesApp', [])
-    .controller('Quote', function($scope, $http, $timeout) {
+(function() {
+    $.ajaxSetup({
+      cache: false
+    });
 
-    var currentQuoteId = undefined;
-    var ts = new Date().getTime();
+    var quote, loading, loadingTimer, more;
+    var currentQuoteId;
 
-    $scope.getRandomQuote = function() {
-        if ($scope.moreDisabled === true) return;
-
+    function getRandomQuoteId() {
         var quoteId;
 
         for (quoteId=Math.floor(Math.random() * LENGTH);
-             currentQuoteId!=undefined && quoteId==currentQuoteId && LENGTH>1;
+             currentQuoteId!=undefined && quoteId===currentQuoteId && LENGTH>1;
              quoteId=Math.floor(Math.random() * LENGTH));
 
-        var url = 'json/' + zfill(quoteId, 5) + '.json?ts=' + ts,
-            loading = $timeout(function() {
-                $scope.quote = undefined;
-            }, 100);
+        return quoteId;
+    }
 
-        $scope.moreDisabled = true;
+    function getQuoteFromHash() {
+        var digits = /\d+/g;
+        var match = location.hash.match(digits);
+        if (match === null) {
+            return null;
+        } else {
+            return parseInt(match[0], 10);
+        }
+    }
 
-        $http.get(url).success(function(data, status, headers, config) {
-            currentQuoteId = quoteId;
-            $scope.moreDisabled = false;
-            $timeout.cancel(loading);
-            $scope.quote = data;
-        });
-    };
-});
+    function zfill(num, size) {
+        var s = num + "";
+        while (s.length < size) s = "0" + s;
+        return s;
+    }
 
-function zfill(num, size) {
-    var s = num + "";
-    while (s.length < size) s = "0" + s;
-    return s;
-}
+    function getQuote() {
+        var quoteId = getQuoteFromHash();
+        if (quoteId === null) {
+            nextQuote();
+            return;
+        }
+        more.hide();
+        loadingTimer = setTimeout(function() {
+            loading.show();
+            quote.hide();
+        }, 100);
+
+        var url = "/json/" + zfill(quoteId, 5) + ".json";
+        $.getJSON(url)
+            .done(function(data) {
+                currentQuoteId = data.release.id;
+                renderQuote(data);
+            })
+            .fail(function() {
+                clearTimeout(loadingTimer);
+                nextQuote();
+            });
+    }
+
+    function nextQuote() {
+        var quoteId = getRandomQuoteId();
+        location.hash = "#/" + quoteId;
+    }
+
+    function renderQuote(data) {
+        var author = quote.find('.author');
+        var release = quote.find('.release');
+        author.text(data.author).prop("class", data.author + ' author');
+        release.find('a').prop('href', data.release.link).text("№ " + data.release.id);
+        release.find('.date').text(data.release.date);
+        release.find('.time').text(data.time);
+        quote.find('.text').text(data.text);
+
+        clearTimeout(loadingTimer);
+        loading.hide();
+        quote.show();
+    }
+
+    window.onhashchange = getQuote;
+    window.nextQuote = nextQuote;
+
+    $(function() {
+        quote = $('#quote, .more');
+        loading = $('#loading');
+        more = $('.more');
+
+        getQuote();
+    });
+
+})();
