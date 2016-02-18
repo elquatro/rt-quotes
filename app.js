bd4fdc5e75fda1241912e78b6c74910bbf9abbbc
elquatro <elquatro@ya.ru>
2016-02-18 11:15:01 +0300

    gen fix

--- app.js
+++ app.js
@@ -1,91 +1,90 @@
-var LENGTH = 68;
-
 (function() {
-    $.ajaxSetup({
-      cache: false
-    });
 
-    var quote, loading, loadingTimer, more;
-    var currentQuoteId;
+$.ajaxSetup({
+  cache: false
+});
 
-    function getRandomQuoteId() {
-        var quoteId;
+var quote, loading, loadingTimer, more;
+var currentQuoteId;
 
-        for (quoteId=Math.floor(Math.random() * LENGTH);
-             currentQuoteId!=undefined && quoteId===currentQuoteId && LENGTH>1;
-             quoteId=Math.floor(Math.random() * LENGTH));
+function getRandomQuoteId() {
+    var quoteId;
 
-        return quoteId;
+    for (quoteId=Math.floor(Math.random() * LENGTH);
+         currentQuoteId!=undefined && quoteId===currentQuoteId && LENGTH>1;
+         quoteId=Math.floor(Math.random() * LENGTH));
+
+    return quoteId;
+}
+
+function getQuoteFromHash() {
+    var digits = /\d+/g;
+    var match = location.hash.match(digits);
+    if (match === null) {
+        return null;
+    } else {
+        return parseInt(match[0], 10);
     }
+}
 
-    function getQuoteFromHash() {
-        var digits = /\d+/g;
-        var match = location.hash.match(digits);
-        if (match === null) {
-            return null;
-        } else {
-            return parseInt(match[0], 10);
-        }
+function zfill(num, size) {
+    var s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function getQuote() {
+    var quoteId = getQuoteFromHash();
+    if (quoteId === null) {
+        nextQuote();
+        return;
     }
+    more.hide();
+    loadingTimer = setTimeout(function() {
+        loading.show();
+        quote.hide();
+    }, 400);
 
-    function zfill(num, size) {
-        var s = num + "";
-        while (s.length < size) s = "0" + s;
-        return s;
-    }
-
-    function getQuote() {
-        var quoteId = getQuoteFromHash();
-        if (quoteId === null) {
+    var url = "json/" + zfill(quoteId, 5) + ".json";
+    $.getJSON(url)
+        .done(function(data) {
+            currentQuoteId = quoteId;
+            renderQuote(data);
+        })
+        .fail(function() {
+            clearTimeout(loadingTimer);
             nextQuote();
-            return;
-        }
-        more.hide();
-        loadingTimer = setTimeout(function() {
-            loading.show();
-            quote.hide();
-        }, 400);
+        });
+}
 
-        var url = "json/" + zfill(quoteId, 5) + ".json";
-        $.getJSON(url)
-            .done(function(data) {
-                currentQuoteId = quoteId;
-                renderQuote(data);
-            })
-            .fail(function() {
-                clearTimeout(loadingTimer);
-                nextQuote();
-            });
-    }
+function nextQuote() {
+    var quoteId = getRandomQuoteId();
+    location.hash = "#/" + quoteId;
+}
 
-    function nextQuote() {
-        var quoteId = getRandomQuoteId();
-        location.hash = "#/" + quoteId;
-    }
+function renderQuote(data) {
+    var author = quote.find('.author');
+    var release = quote.find('.release');
+    author.text(data.author).prop("class", data.author + ' author');
+    release.find('a').prop('href', data.release.link).text("№ " + data.release.id);
+    release.find('.date').text(data.release.date);
+    release.find('.time').text(data.time);
+    quote.find('.text').text(data.text);
 
-    function renderQuote(data) {
-        var author = quote.find('.author');
-        var release = quote.find('.release');
-        author.text(data.author).prop("class", data.author + ' author');
-        release.find('a').prop('href', data.release.link).text("№ " + data.release.id);
-        release.find('.date').text(data.release.date);
-        release.find('.time').text(data.time);
-        quote.find('.text').text(data.text);
+    clearTimeout(loadingTimer);
+    loading.hide();
+    quote.show();
+}
 
-        clearTimeout(loadingTimer);
-        loading.hide();
-        quote.show();
-    }
+window.onhashchange = getQuote;
+window.nextQuote = nextQuote;
 
-    window.onhashchange = getQuote;
-    window.nextQuote = nextQuote;
+$(function() {
+    quote = $('#quote, .more');
+    loading = $('#loading');
+    more = $('.more');
 
-    $(function() {
-        quote = $('#quote, .more');
-        loading = $('#loading');
-        more = $('.more');
-
-        getQuote();
-    });
+    getQuote();
+});
 
 })();
